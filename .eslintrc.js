d42ac0dbd564e0266c58910dc8886ca3cf269ed7
Alberto Vidal <93537515+AVX00@users.noreply.github.com>
2022-01-24 20:48:11 +0100

    copied each class to it's own file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jset: true,
   },
   extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+    "max-classes-per-file": "off",
+  },
 };
---
+++ public/js/asesor.js
@@ -0,0 +1,15 @@
+import Personaje from "./personaje";
+
+export default class Asesor extends Personaje {
+  asesorado;
+  constructor(nombreAsesor, familiaAsesor, edadAsesor, personajeAsesorado) {
+    super(nombreAsesor, familiaAsesor, edadAsesor);
+    if (personajeAsesorado instanceof Personaje) {
+      this.asesorado = personajeAsesorado;
+    }
+  }
+
+  comunicar() {
+    return `${super.comunicar()}No sé por qué, pero creo que voy a morir pronto`;
+  }
+}
