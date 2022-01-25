bc4696ccdebdc3ec9abd8048433265ed9b805d3d
Alberto Vidal <93537515+AVX00@users.noreply.github.com>
2022-01-25 23:37:06 +0100

    added buttons with fuctionality

--- public/js/components/Button.js
+++ public/js/components/Button.js
@@ -1,18 +1,36 @@
 import Component from "./Component.js";
 
 export default class Button extends Component {
-  constructor(parentNode, action, ...options) {
+  constructor(parentNode, text, action) {
     const htmlTag = "button";
     const className = "character__action btn";
 
-    super(parentNode, htmlTag, className);
-
-    Button.on(this.element, action, options);
+    super(parentNode, true, htmlTag, className);
+    this.element.innerText = text;
+    this.on(action);
   }
 
-  static on(element, action, ...options) {
-    element.addEventListerer("click", () => {
-      action(options);
+  on(action) {
+    this.element.addEventListener("click", () => {
+      action();
     });
+  }
+
+  static die(card) {
+    const image = card.querySelector(".character__picture");
+    image.classList.add("character__picture--dead");
+    const thumb = card.querySelector(".fas");
+    thumb.classList.remove("fa-thumbs-up");
+    thumb.classList.add("fa-thumbs-down");
+  }
+
+  static comunicate(personaje) {
+    const comunications = document.querySelector(".comunications");
+    const text = comunications.querySelector("p");
+    comunications.classList.add("on");
+    text.innerText = personaje.comunicar();
+    setTimeout(() => {
+      comunications.classList.remove("on");
+    }, 2000);
   }
 }
